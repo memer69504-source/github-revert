09287fdd22876f60290f6e7df2452590019ad426
arham <marketingwithshopify@gmail.com>
2026-04-14 00:34:31 +0500

    script worjkdone

---
+++ script.js
@@ -0,0 +1,6 @@
+const randomArray = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100) + 1);
+
+console.log(randomArray);
+sadsdfSfA//xcz./vxcz./v./,v,./z./zxv
+mzxca\
+function(){sdasda}
