cbc6276bf9b227d96c9dbc3d70029cc22a6c95d1
arham <marketingwithshopify@gmail.com>
2026-04-14 00:46:53 +0500

    sdasd

--- script.js
+++
@@ -1,6 +0,0 @@
-const randomArray = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100) + 1);
-
-console.log(randomArray);
-sadsdfSfA//xcz./vxcz./v./,v,./z./zxv
-mzxca\
-function(){sdasda}
